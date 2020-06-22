df3bce60152ce0d04379292862dd1a7c527b7e5d
Rub10CP <rubicp1@hotmail.com>
2020-06-22 04:56:56 -0300

    Handling exception and claearing screen

--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -5,7 +5,7 @@ public class BoardException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
 	public BoardException(String msg) {
-		super(msg); //construtor recebe a mensagem.
+		super(msg);  //construtor recebe a mensagem.
 	}
 	
 
